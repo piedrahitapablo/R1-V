e1402af1ae1a3aed7fed4700926211c090e4e5c1
piedrahitapablo <juan.p@snappr.com>
2025-02-25 16:52:25 -0500

    remove uv

--- setup.sh
+++ setup.sh
@@ -2,18 +2,25 @@ set -euxo pipefail
 
 # Install the packages in r1-v .
 cd src/r1-v 
-uv add --dev .
+pip install -e ".[dev]"
 
 cd -
 
 # Addtional modules
-uv add wandb==0.18.3
-uv add tensorboardx
-uv add qwen_vl_utils torchvision
-uv add flash-attn --no-build-isolation
+pip install wandb==0.18.3
+pip install tensorboardx
+pip install qwen_vl_utils torchvision
+pip install flash-attn --no-build-isolation
 
 # vLLM support 
-uv add vllm==0.7.2
+pip install vllm==0.7.2
 
 # fix transformers version
-uv add git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
+pip install git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
+
+
+pip uninstall torch torchvision torchaudio -y
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+pip uninstall flash-attn -y
+pip install "flash-attn @ git+https://github.com/Dao-AILab/flash-attention.git"
